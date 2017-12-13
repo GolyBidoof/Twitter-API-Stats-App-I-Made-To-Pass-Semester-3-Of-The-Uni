38f8dc9f8390818db149c8d606a25f82ed981595
GolyBidoof <1923394+GolyBidoof@users.noreply.github.com>
2017-12-13 22:52:00 +0100

    Beautifying a bit

--- src/JFreeChartBarColors.java
+++ src/JFreeChartBarColors.java
@@ -21,19 +21,20 @@ import twitter4j.TwitterException;
 import org.jfree.chart.ChartUtils;
 public class JFreeChartBarColors {
 
-	public static void color(Map<String, Long> sortedMap) throws FontFormatException, IOException {
+	public static void color(Map < String, Long > sortedMap) throws FontFormatException,
+	IOException {
 
 		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 
-		for ( Map.Entry<String, Long> entry : sortedMap.entrySet()) {
-		    String key = entry.getKey();
-		    Long val = entry.getValue();
-		    dataset.addValue((double) val, "Language uses", key);
+		for (Map.Entry < String, Long > entry: sortedMap.entrySet()) {
+			String key = entry.getKey();
+			Long val = entry.getValue();
+			dataset.addValue((double) val, "Language uses", key);
 		}
 
 		/* create chart */
 		JFreeChart chart = ChartFactory.createBarChart("Breakdown of the last 100 Tweets by Fyne-Q by language posted", "Language", "Amount of Tweets", dataset);
-		
+
 		/* Get instance of CategoryPlot */
 		CategoryPlot plot = chart.getCategoryPlot();
 		chart.removeLegend();
@@ -42,45 +43,45 @@ public class JFreeChartBarColors {
 		BarRenderer renderer = (BarRenderer) plot.getRenderer();
 		renderer.setBarPainter(new StandardBarPainter());
 		/* Change font */
-		
-        File f = new File("src/Ubuntu.ttf");
-        File f1 = new File("src/Ubuntu-Bold.ttf");
-        Font ubuntuFont = Font.createFont(Font.TRUETYPE_FONT, f);
-        Font ubuntuFontBold = Font.createFont(Font.TRUETYPE_FONT, f1);
-        ubuntuFont = ubuntuFont.deriveFont(20f);
-        
-        ValueAxis axis = plot.getRangeAxis();
-        axis.setTickLabelFont(ubuntuFont);
-        CategoryAxis axis2 = plot.getDomainAxis();
-        axis2.setTickLabelFont(ubuntuFont);
-        
-        ubuntuFontBold = ubuntuFontBold.deriveFont(28f);
-        plot.getDomainAxis().setLabelFont(ubuntuFontBold);
-        plot.getRangeAxis().setLabelFont(ubuntuFontBold);
+
+		File f = new File("src/Ubuntu.ttf");
+		File f1 = new File("src/Ubuntu-Bold.ttf");
+		Font ubuntuFont = Font.createFont(Font.TRUETYPE_FONT, f);
+		Font ubuntuFontBold = Font.createFont(Font.TRUETYPE_FONT, f1);
+		ubuntuFont = ubuntuFont.deriveFont(20f);
+
+		ValueAxis axis = plot.getRangeAxis();
+		axis.setTickLabelFont(ubuntuFont);
+		CategoryAxis axis2 = plot.getDomainAxis();
+		axis2.setTickLabelFont(ubuntuFont);
+
+		ubuntuFontBold = ubuntuFontBold.deriveFont(28f);
+		plot.getDomainAxis().setLabelFont(ubuntuFontBold);
+		plot.getRangeAxis().setLabelFont(ubuntuFontBold);
 		chart.getTitle().setFont(ubuntuFontBold);
 
 		// set the color (r,g,b) or (r,g,b,a)
 		Color color = new Color(79, 129, 189);
-		
+
 		renderer.setSeriesPaint(0, color);
-		
+
 		//renderer.setSeriesPaint(0, Color.blue);
 		renderer.setDrawBarOutline(true);
 		renderer.setItemMargin(0);
 
-		OutputStream out = null; 
-		
-		try { 
-			out = new FileOutputStream(new File("data/chart.png")); 
-			ChartUtils.writeChartAsPNG(out, chart, 1280, 720); 
-		} catch (IOException e) { 
+		OutputStream out = null;
+
+		try {
+			out = new FileOutputStream(new File("data/chart.png"));
+			ChartUtils.writeChartAsPNG(out, chart, 1280, 720);
+		} catch(IOException e) {
 			e.printStackTrace();
 		}
 		File postableImage = new File("data/chart.png");
-		/*try {
+		try {
 			TwitterAPIPostingActions.writeATweetContainingAPic("Here's some stats for Fyne-Q", postableImage);
-		} catch (TwitterException e) {
+		} catch(TwitterException e) {
 			e.printStackTrace();
-		}*/
+		}
 	}
 }
--- src/loginTest.java
+++ src/loginTest.java
@@ -1,94 +1,72 @@
-import twitter4j.Twitter;
+﻿import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
- 
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
- 
+
 public final class loginTest {
-		static Twitter twitter = TwitterFactory.getSingleton();
-		public static void main(String[] args) throws Exception {
-		  	//Message Contents
-		    
-			twitter.setOAuthConsumer(keys.keyPublic, keys.keySecret);
-		    
-		    //Check if the token file exists
-		    File file = new File("token");
-		    AccessToken accessToken = null;
-		    
-		    if (file.exists()) {
-		    	//Basically set up the file to the old data
-		    	BufferedReader reader = new BufferedReader(new FileReader(file));
-			    String text = null;
-			    String token = null;
-			    String tokenSecret = null;
-			    if ((text = reader.readLine()) != null) token = text;
-			    if ((text = reader.readLine()) != null) tokenSecret = text;
-			    if (token != null && tokenSecret != null) {
-			    	accessToken=new AccessToken(token, tokenSecret);
-			    	twitter.setOAuthAccessToken(accessToken);
-			    } else {
-			    	System.out.println("Invalid token!");
-			    	System.exit(0);
-			    }
-			    reader.close();
-		    } else {
-		    	//Request to obtain the token
-		    	RequestToken requestToken = twitter.getOAuthRequestToken();
-			    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			    while (null == accessToken) {
-				      System.out.println("Copy this link to your browser to get a token");
-				      System.out.println(requestToken.getAuthorizationURL());
-				      System.out.print("And now just enter PIN: ");
-				      String pin = br.readLine();
-				      try{
-					         if(pin.length() > 0){
-					           accessToken = twitter.getOAuthAccessToken(requestToken, pin);
-					         } else {
-					           accessToken = twitter.getOAuthAccessToken();
-					         }
-					         BufferedWriter writer = new BufferedWriter(new FileWriter("token"));
-					         writer.write(accessToken.getToken());
-					         writer.newLine();
-					         writer.append(accessToken.getTokenSecret());
-					         writer.close();
-				      } catch (TwitterException te) {
-					        if(401 == te.getStatusCode()){
-					          System.out.println("Unable to get the access token.");
-					        } else {
-					          te.printStackTrace();
-					        }
-				      }
-			    }
-		    }
-		    
-		    
-		    AnalyzeLang.analyzeLang();
-		    
-		    
-		    //String output = "She wrote " + polcount + " tweets in Polish and " + engcount + " tweets in English";
-		    //TwitterAPIPostingActions.writeATextTweet(output);
-		    //User username = twitter.showUser("szymbar15");
-		    //Status status = twitter.updateStatus("My creator Szymbar has " + username.getFollowersCount() + " followers, " + username.getStatusesCount() + " tweets, he uses " + username.getLang() + " language and claims he's from " + username.getLocation() + ".");
-		    //System.out.println("Successfully updated the status to [" + status.getText() + "].");
-		    
-		    //Status status2 = twitter.updateStatus("BAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-		    //System.out.println("Successfully updated the status to [" + status2.getText() + "].");
-		    /*System.out.println("Showing home timeline.");
-		    for (Status status : statuses) {
-		        System.out.println(status.getUser().getName() + ":" + status.getCreatedAt() + ":" +
-		                           status.getText());
-		    }*/
-		    
-		    
-		    System.exit(0);
-		  }
-	  
-	  
+	static Twitter twitter = TwitterFactory.getSingleton();
+	public static void main(String[] args) throws Exception {
+		//Message Contents
+		twitter.setOAuthConsumer(keys.keyPublic, keys.keySecret);
+
+		//Check if the token file exists
+		File file = new File("token");
+		AccessToken accessToken = null;
+
+		if (file.exists()) {
+			//Basically set up the file to the old data
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			String text = null;
+			String token = null;
+			String tokenSecret = null;
+			if ((text = reader.readLine()) != null) token = text;
+			if ((text = reader.readLine()) != null) tokenSecret = text;
+			if (token != null && tokenSecret != null) {
+				accessToken = new AccessToken(token, tokenSecret);
+				twitter.setOAuthAccessToken(accessToken);
+			} else {
+				System.out.println("Invalid token!");
+				System.exit(0);
+			}
+			reader.close();
+		} else {
+			//Request to obtain the token
+			RequestToken requestToken = twitter.getOAuthRequestToken();
+			BufferedReader br = new BufferedReader(new InputStreamReader(System. in ));
+			while (null == accessToken) {
+				System.out.println("Copy this link to your browser to get a token");
+				System.out.println(requestToken.getAuthorizationURL());
+				System.out.print("And now just enter PIN: ");
+				String pin = br.readLine();
+				try {
+					if (pin.length() > 0) {
+						accessToken = twitter.getOAuthAccessToken(requestToken, pin);
+					} else {
+						accessToken = twitter.getOAuthAccessToken();
+					}
+					BufferedWriter writer = new BufferedWriter(new FileWriter("token"));
+					writer.write(accessToken.getToken());
+					writer.newLine();
+					writer.append(accessToken.getTokenSecret());
+					writer.close();
+				} catch(TwitterException te) {
+					if (401 == te.getStatusCode()) {
+						System.out.println("Unable to get the access token.");
+					} else {
+						te.printStackTrace();
+					}
+				}
+			}
+		}
+		AnalyzeLang.analyzeLang();
+		System.exit(0);
+	}
 }
